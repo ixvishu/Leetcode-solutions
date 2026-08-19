6ab1fc39b6be4e13fa4a2fc85ebbcec520f4d26c
ixvishu <121726473+ixvishu@users.noreply.github.com>
2026-08-19 12:54:11 +0530

    Time: 11 ms (51.15%), Space: 19.1 MB (96.19%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,20 +1,20 @@
-1class Solution:
-2    def convert(self, s: str, numRows: int) -> str:
-3        if numRows == 1 or numRows >= len(s):
-4            return s
-5
-6        rows = [""] * numRows
-7        currRow = 0
-8        direction = -1
-9
-10        for ch in s:
-11            rows[currRow] += ch
-12
-13            if currRow == 0 or currRow == numRows - 1:
-14                direction *= -1
-15
-16            currRow += direction
-17
-18        return "".join(rows)
-19        
-20
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        currRow = 0
+        direction = -1
+
+        for ch in s:
+            rows[currRow] += ch
+
+            if currRow == 0 or currRow == numRows - 1:
+                direction *= -1
+
+            currRow += direction
+
+        return "".join(rows)
+        
+        
